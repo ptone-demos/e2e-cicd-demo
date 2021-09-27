519f7f3764584244044f8a4d06fa9ffadd968933
Preston Holmes <ptone@google.com>
2021-09-27 20:39:29 +0000

    fix deploy

--- example/leeroy-web/web.go
+++ example/leeroy-web/web.go
@@ -19,7 +19,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	log.Print("Cloud Next 2021! - leeroy web server ready! change three")
+	log.Print("Cloud Next 2021! - leeroy web server ready! change fix")
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
 }
